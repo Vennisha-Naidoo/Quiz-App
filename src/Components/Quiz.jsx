ea42e045b1e9628a4f34d96ff80f8a93f6851c9f
Vennisha-Naidoo <vennisha31@gmail.com>
2024-11-29 16:07:46 +0200

    Quiz Completed

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react"
 import QUESTIONS from '../questions.js';
-
+import quizCompleted from '../assets/quiz-complete.png';
 
 export default function Quiz() {
 
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-    shuffledAnswers.sort(() => Math.random() - 0.5);
 
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
@@ -19,19 +17,33 @@ export default function Quiz() {
         });
     }
 
-    return <div id="quiz">
-        <div id="question">
-            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-            <ul id="answers">
-                {
-                    shuffledAnswers.map((answer) => (
-                        <li key={answer} className="answer">
-                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                        </li>
-                    ))
-                }
-            </ul>
+    if (quizIsComplete) {
+        return (
+            <div id="summary">
+                <img src={quizCompleted} alt="Trophy Icon" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        )
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
+    return (
+        <div id="quiz">
+            <div id="question">
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id="answers">
+                    {
+                        shuffledAnswers.map((answer) => (
+                            <li key={answer} className="answer">
+                                <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
         </div>
-    </div>
+    )
 
 }
